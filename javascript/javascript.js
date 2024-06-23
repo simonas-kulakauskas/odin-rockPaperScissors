4626081dbaca7fb224a545fa77fd0db6c5225a29
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-23 21:59:02 +0100

    General Fix up and removal of waste. - Cleaned up comments and added better. - Removed unused functions - Reformatted JS to use less variables as well as better sectioned for clarity. - Removed some of the old console.log() messages used to tell player who won game/round.
    READY TO MERGE :)

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,23 +2,9 @@ let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
-// TODO: Reduce the size of these, no need to have them as variables and to have the event listener call an anonymous function when we can pass the 'playRound' 
-// TODO: Straight away....
+const getRandomInt = () => Math.floor(Math.random() * 3);
 
-// * Displaying Function
-
-const messageDisplay = document.getElementById("messageDisplay");
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");
-const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-const drawScoreDisplay = document.getElementById("drawScoreDisplay");
-
-
-
-
-const getRandomInt = () => Math.floor(Math.random() * 3); // To get a random choice of 3 (for rock, paper or scissors)
-
-
-const getComputerChoice = () => { // Gets response from getRandomInt and choose the corresponding rock, paper or scissors.
+const getComputerChoice = () => {
     switch (getRandomInt()) {
         case 0: return ("rock");
         case 1: return ("paper");
@@ -30,28 +16,8 @@ const getComputerChoice = () => { // Gets response from getRandomInt and choose 
     }
 }
 
-const promptHuman = () => window.prompt("Pick: Rock, Paper, Scissors...", "");
-
-
-const getHumanChoice = () => { // Force player to pick one of 3, make sure valid otherwise let them retry.
-    const choices = ["rock", "paper", "scissors"];
-    let humanChoice;
-
-    humanChoice = promptHuman().toLowerCase();
-
-    if (choices.includes(humanChoice)) {
-        return (humanChoice);
-    }
-    else {
-        window.alert(humanChoice + " is not valid, try again...")
-        getHumanChoice();
-    }
-
-}
-
-
 const checkWhoWon = (humanChoice, computerChoice) => { // Checks who won the round, returns 0 for draw, 1 for human, 2 for computer.
-    if (humanChoice === computerChoice) { // Check draw?
+    if (humanChoice === computerChoice) { // Draw?
         return 0;
     }
     else if (humanChoice === "rock") {
@@ -69,15 +35,39 @@ const checkWhoWon = (humanChoice, computerChoice) => { // Checks who won the rou
 
 }
 
-const stopAllListeners = () => {
+const playRound = (humanChoice, computerChoice) => { // Run 'checkWhoWon', display message and change score based on who won.
+    let winner = checkWhoWon(humanChoice, computerChoice);
+    switch (winner) {
+        case 0: // draw case
+            drawScore++;
+            updateDisplay(0);
+            break;
+        case 1: // human win case
+            humanScore++;
+            updateDisplay(1);
+            break;
+        case 2: // computer win case
+            computerScore++;
+            updateDisplay(2);
+            break;
+    }
+}
+
+
+// Grab elements that'll be changed from dom.
+const messageDisplay = document.getElementById("messageDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+const drawScoreDisplay = document.getElementById("drawScoreDisplay");
+
+const stopAllListeners = () => { // Stops buttons working when game is won.
     rockButton.removeEventListener("click", rockButtonHandler);
     paperButton.removeEventListener("click", paperButtonHandler);
     scissorsButton.removeEventListener("click", scissorsButtonHandler);
 }
 
-const updateDisplay = (winner) => {
+const updateDisplay = (winner) => { // Update's display if noone's won, updates display and stops listeners if someone won
     if (humanScore === 5) {
-        console.log("HUMAN 5 ROUNDS")
         messageDisplay.textContent = "PLAYER HAS REACHED 5 POINTS, PLAYER WINS!";
         playerScoreDisplay.textContent = 5;
         stopAllListeners();
@@ -104,73 +94,21 @@ const updateDisplay = (winner) => {
         }
     }
 
-
-
-
 }
 
+// Button listeners and handlers.
 
-
-const playRound = (humanChoice, computerChoice) => { // Run 'checkWhoWon', display message and change score based on who won.
-    let winner = checkWhoWon(humanChoice, computerChoice);
-    switch (winner) {
-        case 0: // draw case
-            drawScore++;
-            updateDisplay(0);
-            console.log("Draw, both selected " + humanChoice + ", score left unchanged.");
-            break;
-        case 1: // human win case
-            humanScore++;
-            updateDisplay(1);
-            console.log("Human wins!, selected " + humanChoice + ", Computer selected " + computerChoice + ".");
-            break;
-        case 2: // computer win case
-            computerScore++;
-            updateDisplay(2);
-            console.log("Computer wins!, selected " + computerChoice + ", Human selected " + humanChoice + ".");
-            break;
-    }
-}
-
-// * Buttons functions
-const rockButton = document.getElementById("rockButton");
-
-
-// rockButton.addEventListener("click", () => {
-//     
-// });
 function rockButtonHandler() {
     playRound("rock", getComputerChoice());
 }
-
-rockButton.addEventListener("click", rockButtonHandler);
-
-
-const paperButton = document.getElementById("paperButton");
+document.getElementById("rockButton").addEventListener("click", rockButtonHandler);
 
 function paperButtonHandler() {
     playRound("paper", getComputerChoice());
 }
-
-paperButton.addEventListener("click", paperButtonHandler);
-
-const scissorsButton = document.getElementById("scissorsButton");
+document.getElementById("paperButton").addEventListener("click", paperButtonHandler);
 
 function scissorsButtonHandler() {
     playRound("scissors", getComputerChoice());
 }
-
-scissorsButton.addEventListener("click", scissorsButtonHandler);
-
-
-
-
-
-
-
-// playGame();
-
-
-
-
-
+document.getElementById("scissorsButton").addEventListener("click", scissorsButtonHandler);
